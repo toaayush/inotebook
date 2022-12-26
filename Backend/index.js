50bf3520b909861662cbb2615dada7eae50f0ae1
toaayush <toaayush@users.noreply.github.com>
2022-12-26 11:56:23 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,12 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello Ayush!')
 })
+app.get('/api/v1/login', (req, res) => {
+  res.send('Hello login!')
+})
+app.get('/api/v1/signup', (req, res) => {
+  res.send('Hello signup!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
