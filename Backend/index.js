4e59f5cb504ecad978b921b4e8b23c97b6ce29df
toaayush <toaayush@users.noreply.github.com>
2022-12-26 12:51:10 +0530

    routes created

--- Backend/index.js
+++ Backend/index.js
@@ -5,16 +5,13 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+// Available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 app.get('/', (req, res) => {
   res.send('Hello Ayush!')
 })
-app.get('/api/v1/login', (req, res) => {
-  res.send('Hello login!')
-})
-app.get('/api/v1/signup', (req, res) => {
-  res.send('Hello signup!')
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
